302c18a74b2d8a0c49ec8dfed773a6dae43daafb
Zulaikha Ashiq <zulaikha.a@jitglobalinfosystems.com>
2026-01-19 11:12:39 +0530

    replace vanila style with shadcn comp

--- FrontEnd/src/components/ui/input.jsx
+++ FrontEnd/src/components/ui/input.jsx
@@ -1,27 +1,19 @@
-export function Input({
-    label,
-    value,
-    onChange,
-    placeholder,
-    type = "text",
-    required = false,
-  }) {
-    return (
-      <div className="flex flex-col gap-1">
-        {label && (
-          <label className="text-sm font-medium">
-            {label} {required && <span className="text-red-500">*</span>}
-          </label>
-        )}
-  
-        <input
-          type={type}
-          value={value}
-          onChange={onChange}
-          placeholder={placeholder}
-          className="border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-black"
-        />
-      </div>
-    )
-  }
-  
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+const Input = React.forwardRef(({ className, type, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={cn(
+        "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        className
+      )}
+      ref={ref}
+      {...props} />
+  );
+})
+Input.displayName = "Input"
+
+export { Input }
